be67854350c3500e09a580cc5bcbb166fdf38e28
Alexander Jacks and Stewart Cole <stewart@>
2016-08-02 13:58:04 -0700

    builds asset pipelinne that collects Open Weather data via API

--- build/js/app.js
+++ build/js/app.js
@@ -90,4 +90,24 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $('#time').text(moment());
+});
+
+// a45ffef5ad9f6b6488d6272af08efb1b
+// http://api.openweathermap.org/data/2.5/weather?q=Portland,us&appid=a45ffef5ad9f6b6488d6272af08efb1b
+var apiKey = "a45ffef5ad9f6b6488d6272af08efb1b";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+
+    });
+  });
+});
+
 },{"./../js/pingpong.js":1}]},{},[2]);
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -52,3 +52,23 @@ $(document).ready(function(){
     $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
   });
 });
+
+$(document).ready(function(){
+  $('#time').text(moment());
+});
+
+// a45ffef5ad9f6b6488d6272af08efb1b
+// http://api.openweathermap.org/data/2.5/weather?q=Portland,us&appid=a45ffef5ad9f6b6488d6272af08efb1b
+var apiKey = "a45ffef5ad9f6b6488d6272af08efb1b";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(response);
+
+    });
+  });
+});
